a16af8f12b1287e952626cdf5292a74614a31be9
hollyjanedalton <hjdalton89@gmail.com>
2022-04-21 14:20:49 +0200

    Integrated search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello there</header>
+      <div className="container">
+        <header className="App-header">Dictionary</header>
+        <a href="/" className="btn btn-warning">
+          Emergency!
+        </a>
+        <main>
+          {" "}
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+          {" "}
+          <small>Coded by Holly Dalton</small>
+        </footer>
+      </div>
     </div>
   );
 }
