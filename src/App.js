8d86a0e8effeed5fda8e97a221d30249337a93ca
hollyjanedalton <hjdalton89@gmail.com>
2022-04-21 14:25:13 +0200

    Updated search engine css

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">Dictionary</header>
-        <a href="/" className="btn btn-warning">
-          Emergency!
-        </a>
+        <header className="App-header">
+          {" "}
+          <strong>Dictionary</strong>
+        </header>
+
         <main>
           {" "}
           <Dictionary />
