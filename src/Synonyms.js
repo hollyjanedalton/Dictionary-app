e52410db2ae0906a731adc87eb97deee571f1832
hollyjanedalton <hjdalton89@gmail.com>
2022-04-28 14:20:41 +0200

    Style the application

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,11 +1,11 @@
 import React from "react";
+import "./Synonyms.css";
 
 export default function Synonyms(props) {
   if (props.synonyms) {
     return (
       <div className="Synonyms">
         <ul>
-          Synonyms:
           {props.synonyms.map(function (synonym, index) {
             return <li key={index}>{synonym}</li>;
           })}
